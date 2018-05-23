8ebfe3cfefbc2fdb7379c58e1e81c5739b6cb35e
Leonardo Razovic <leorazzo@gmail.com>
2018-05-23 22:20:53 +0200

    Better Config

--- common.h
+++ common.h
@@ -1,31 +1,27 @@
 #ifndef COMMON_H
 #define COMMON_H
-
+#include "errno.h"
 #include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
 
-// Error handling
-#define GENERIC_ERROR_HELPER(cond, errCode, msg) do {               \
-        if (cond) {                                                 \
-            fprintf(stderr, "%s: %s\n", msg, strerror(errCode));    \
-            exit(EXIT_FAILURE);                                     \
-        }                                                           \
-    } while(0)
+// macro to simplify error handling
+#define GENERIC_ERROR_HELPER(cond, errCode, msg)                 \
+    do {                                                         \
+        if (cond) {                                              \
+            fprintf(stderr, "%s: %s\n", msg, strerror(errCode)); \
+            exit(EXIT_FAILURE);                                  \
+        }                                                        \
+    } while (0)
 
-#define ERROR_HELPER(ret, msg)          GENERIC_ERROR_HELPER((ret < 0), errno, msg)
-#define PTHREAD_ERROR_HELPER(ret, msg)  GENERIC_ERROR_HELPER((ret != 0), ret, msg)
+#define ERROR_HELPER(ret, msg) GENERIC_ERROR_HELPER((ret < 0), errno, msg)
+#define PTHREAD_ERROR_HELPER(ret, msg) GENERIC_ERROR_HELPER((ret != 0), ret, msg)
 
 // Config
-#define MAX_CONN_QUEUE  3   // max number of connections the server can queue
-#define DEBUG           1 
-#define SERVER_ADDRESS  "127.0.0.1"
-#define SERVER_COMMAND  "QUIT"
-#define NUM_MSG     10
-#define BUFFERSIZE 8888888
-#define MAX_NUM     (1 << 16)
-#define BUF_SIZE    8
-#define SEED        0
+#define DEBUG 1 
+#define BUFFER_SIZE 1000000
+#define TCP_PORT 8888
+#define UDP_PORT 8888 
+#define SERVER_ADDRESS "127.0.0.1"
+#define TIME_TO_SLEEP 10000
+#define WORLD_SIZE 10
+
 #endif
