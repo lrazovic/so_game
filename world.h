5f8e43e182d828bd0361d6dbc96b4178a4e6ef31
Leonardo Razovic <leorazzo@gmail.com>
2018-05-23 22:40:33 +0200

    clang-format for better reading

--- world.h
+++ world.h
@@ -6,24 +6,28 @@
 #include <sys/time.h>
 
 typedef struct World {
-  ListHead vehicles; // list of vehicles
-  Surface ground;    // surface
+    ListHead vehicles; // list of vehicles
+    Surface ground; // surface
 
-  // stuff
-  float dt;
-  struct timeval last_update;
-  float time_scale;
+    // stuff
+    float dt;
+    struct timeval last_update;
+    float time_scale;
 } World;
 
-int World_init(World *w, Image *surface_elevation, Image *surface_texture,
-               float x_step, float y_step, float z_step);
+int World_init(World* w,
+    Image* surface_elevation,
+    Image* surface_texture,
+    float x_step,
+    float y_step,
+    float z_step);
 
-void World_destroy(World *w);
+void World_destroy(World* w);
 
-void World_update(World *w);
+void World_update(World* w);
 
-Vehicle *World_getVehicle(World *w, int vehicle_id);
+Vehicle* World_getVehicle(World* w, int vehicle_id);
 
-Vehicle *World_addVehicle(World *w, Vehicle *v);
+Vehicle* World_addVehicle(World* w, Vehicle* v);
 
-Vehicle *World_detachVehicle(World *w, Vehicle *v);
+Vehicle* World_detachVehicle(World* w, Vehicle* v);
