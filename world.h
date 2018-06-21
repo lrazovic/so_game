cf26e85ebe71a29a0df484327d55d89698196fa5
Leonardo Razovic <leorazzo@gmail.com>
2018-06-21 11:51:46 +0200

    Cleaning of world structs

--- world.h
+++ world.h
@@ -1,33 +1,30 @@
 #pragma once
-#include "image.h"
-#include "linked_list.h"
-#include "surface.h"
-#include "vehicle.h"
 #include <sys/time.h>
+#include "linked_list.h"
+#include "vehicle.h"
+#include "image.h"
+#include "surface.h"
 
 typedef struct World {
-    ListHead vehicles; // list of vehicles
-    Surface ground; // surface
-
-    // stuff
-    float dt;
-    struct timeval last_update;
-    float time_scale;
+  ListHead vehicles; 
+  Surface ground;  
+  float dt;
+  struct timeval last_update;
+  float time_scale;
 } World;
 
-int World_init(World* w,
-    Image* surface_elevation,
-    Image* surface_texture,
-    float x_step,
-    float y_step,
-    float z_step);
+int World_init(World* w, Image* surface_elevation, Image* surface_texture);
 
 void World_destroy(World* w);
 
 void World_update(World* w);
+
+void World_decayUpdate(World* w);
 
 Vehicle* World_getVehicle(World* w, int vehicle_id);
 
 Vehicle* World_addVehicle(World* w, Vehicle* v);
 
 Vehicle* World_detachVehicle(World* w, Vehicle* v);
+
+void World_manualUpdate(World* w, Vehicle* v, struct timeval update_time);
